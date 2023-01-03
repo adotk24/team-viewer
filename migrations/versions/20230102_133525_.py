ebd758f6ce96c8df68d857b593bc6e678c2ef660
Andrew <andrewkim1998@gmail.com>
2023-01-03 10:44:19 -0600

    putting missing code in alembic versions

--- migrations/versions/20230102_133525_.py
+++ migrations/versions/20230102_133525_.py
@@ -7,7 +7,9 @@ Create Date: 2023-01-02 13:35:25.141000
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '61a427b8d29c'
