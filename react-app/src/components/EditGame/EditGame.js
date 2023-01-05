dc8327d73b138683d4396cba5ea7802da983581e
Andrew <andrewkim1998@gmail.com>
2023-01-04 18:02:33 -0600

    edit game form bug fixed

--- react-app/src/components/EditGame/EditGame.js
+++ react-app/src/components/EditGame/EditGame.js
@@ -1,3 +1,117 @@
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux"
+// import { NavLink, useParams, useHistory } from "react-router-dom";
+// import './EditGame.css'
+// import { edittingGame, getOneGame } from "../../store/game";
+
+// const EditGame = () => {
+//     const { gameId } = useParams()
+//     const history = useHistory()
+//     const dispatch = useDispatch()
+//     const findGame = useSelector(state => state.game.oneGame)
+//     const [loaded, setLoaded] = useState(false)
+//     let [values, setValues] = useState(false)
+//     let [datetime, setDatetime] = useState('')
+//     useEffect(() => {
+//         dispatch(getOneGame(gameId)).then(() => {
+//             setLoaded(true)
+//             if (findGame && findGame.Game) {
+//                 const game = findGame.Game[0]
+//                 const gameSplit = game.datetime.split(' ')
+//                 const day = gameSplit[1]
+//                 const month = getMonth(gameSplit[2])
+//                 const year = gameSplit[3]
+//                 const time = gameSplit[4]
+//                 const datetime = (`${year}-${month}-${day}T${time}`)
+//                 setDatetime(datetime)
+//                 setValues(true)
+//             }
+//         })
+//     }, [dispatch, findGame])
+
+
+
+//     const getMonth = (str) => {
+//         if (str == 'Jan') return '01'
+//         if (str == 'Feb') return '02'
+//         if (str == 'Mar') return '03'
+//         if (str == 'Apr') return '04'
+//         if (str == 'May') return '05'
+//         if (str == 'Jun') return '06'
+//         if (str == 'Jul') return '07'
+//         if (str == 'Aug') return '08'
+//         if (str == 'Sep') return '09'
+//         if (str == 'Oct') return '10'
+//         if (str == 'Nov') return '11'
+//         if (str == 'Dec') return '12'
+//     }
+
+//     return loaded && values && (
+//         < div className="game-form-container" >
+//             <form className="game-form">
+//                 <input
+//                     type='datetime-local'
+//                     placeholder='datetime'
+//                     value={datetime}
+//                     onChange={(e) => setDatetime(e.target.value)}
+//                 ></input>
+//             </form>
+//         </div >
+//     )
+// }
+
+// export default EditGame
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux"
+// import { NavLink, useParams, useHistory } from "react-router-dom";
+// import './EditGame.css'
+// import { edittingGame, getOneGame } from "../../store/game";
+
+// const EditGame = () => {
+//     const { gameId } = useParams()
+//     const history = useHistory()
+//     const dispatch = useDispatch()
+//     const findGame = useSelector(state => state.game.oneGame)
+//     let [datetime, setDatetime] = useState(null)
+
+//     useEffect(() => {
+//         dispatch(getOneGame(gameId))
+//     }, [gameId])
+
+//     useEffect(() => {
+//         setDatetime(findGame && findGame.Game ? findGame.Game[0].datetime : null)
+//     }, [findGame])
+
+//     const getMonth = (str) => {
+//         if (str == 'Jan') return '01'
+//         if (str == 'Feb') return '02'
+//         if (str == 'Mar') return '03'
+//         if (str == 'Apr') return '04'
+//         if (str == 'May') return '05'
+//         if (str == 'Jun') return '06'
+//         if (str == 'Jul') return '07'
+//         if (str == 'Aug') return '08'
+//         if (str == 'Sep') return '09'
+//         if (str == 'Oct') return '10'
+//         if (str == 'Nov') return '11'
+//         if (str == 'Dec') return '12'
+//     }
+
+//     return datetime && (
+//         < div className="game-form-container" >
+//             <form className="game-form">
+//                 <input
+//                     type='datetime-local'
+//                     placeholder='datetime'
+//                     value={datetime}
+//                     onChange={(e) => setDatetime(e.target.value)}
+//                 ></input>
+//             </form>
+//         </div >
+//     )
+// }
+
+// export default EditGame
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { NavLink, useParams, useHistory } from "react-router-dom";
@@ -9,32 +123,25 @@ const EditGame = () => {
     const history = useHistory()
     const dispatch = useDispatch()
     const findGame = useSelector(state => state.game.oneGame)
-    const [loaded, setLoaded] = useState(false)
-    let [values, setValues] = useState(false)
+    let [datetime, setDatetime] = useState(null)
+
     useEffect(() => {
-        dispatch(getOneGame(gameId)).then(() => {
-            setLoaded(true)
-            if (findGame && findGame.Game) {
-                const game = findGame.Game[0]
-                const gameSplit = game.datetime.split(' ')
-                const day = gameSplit[1]
-                const month = getMonth(gameSplit[2])
-                const year = gameSplit[3]
-                const time = gameSplit[4]
-                const datetime = (`${year}-${month}-${day}T${time}`)
-                setDatetime(datetime)
-                setValues(true)
-            }
-        })
+        dispatch(getOneGame(gameId))
     }, [gameId])
-    // useEffect(() => {
-    //     dispatch(getOneGame(gameId)).then(() => {
-    //         isLoaded(true)
-    //     })
-    // }, [gameId])
 
+    useEffect(() => {
+        if (findGame && findGame.Game) {
+            const game = findGame.Game[0]
+            const gameSplit = game.datetime.split(' ')
+            const day = gameSplit[1]
+            const month = getMonth(gameSplit[2])
+            const year = gameSplit[3]
+            const time = gameSplit[4]
+            const datetime = (`${year}-${month}-${day}T${time}`)
+            setDatetime(datetime)
+        }
+    }, [findGame])
 
-    let [datetime, setDatetime] = useState('2020-01-01T00:00')
     const getMonth = (str) => {
         if (str == 'Jan') return '01'
         if (str == 'Feb') return '02'
@@ -50,27 +157,7 @@ const EditGame = () => {
         if (str == 'Dec') return '12'
     }
 
-    // useEffect(() => {
-    //     setDatetime(datetime)
-    // }, [datetime])
-
-
-    // if (loaded && !values) {
-    //     game = findGame.Game[0]
-    //     const gameSplit = game.datetime.split(' ')
-    //     day = gameSplit[1]
-    //     month = getMonth(gameSplit[2])
-    //     year = gameSplit[3]
-    //     time = gameSplit[4]
-    //     datetime = (`${year}-${month}-${day}T${time}`)
-    //     console.log('VALUES', values)
-    //     setValues(true)
-    //     console.log('VALUES POST', values)
-    // }
-
-
-    console.log('RIGHT BEFORE RETURN', datetime)
-    return loaded && values && (
+    return datetime && (
         < div className="game-form-container" >
             <form className="game-form">
                 <input
